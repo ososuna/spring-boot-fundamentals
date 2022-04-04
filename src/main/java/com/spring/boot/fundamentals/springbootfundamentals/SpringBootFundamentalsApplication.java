6f941f692d6f4f15f0394fb75068ac1edd3db211
oswi <oswaldo.adrian@live.com.mx>
2022-04-04 18:44:13 -0500

    create spring boot project

---
+++ src/main/java/com/spring/boot/fundamentals/springbootfundamentals/SpringBootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.fundamentals.springbootfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/fundamentals/springbootfundamentals/SpringBootFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.fundamentals.springbootfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
