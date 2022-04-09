b78ecb3bf3b77f42a1cab70d1f77228ccda10c58
oswi <oswaldo.adrian@live.com.mx>
2022-04-08 20:08:51 -0500

    h2 config

--- src/main/java/com/spring/boot/fundamentals/springbootfundamentals/configuration/GeneralConfiguration.java
+++ src/main/java/com/spring/boot/fundamentals/springbootfundamentals/configuration/GeneralConfiguration.java
@@ -1,11 +1,14 @@
 package com.spring.boot.fundamentals.springbootfundamentals.configuration;
 
+import javax.sql.DataSource;
+
 import com.spring.boot.fundamentals.springbootfundamentals.bean.MyBeanWithProperties;
 import com.spring.boot.fundamentals.springbootfundamentals.bean.MyBeanWithPropertiesImplement;
 import com.spring.boot.fundamentals.springbootfundamentals.pojo.UserPojo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +28,19 @@ public class GeneralConfiguration {
   @Bean
   public MyBeanWithProperties function() {
     return new MyBeanWithPropertiesImplement(name, lastName);
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    
+    DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+    
+    dataSourceBuilder.driverClassName("org.h2.Driver");
+    dataSourceBuilder.url("jdbc:h2:mem:testdb");
+    dataSourceBuilder.username("sa");
+    dataSourceBuilder.password("");
+    
+    return dataSourceBuilder.build();
   }
 
 }
